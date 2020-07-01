b19a3ece216d6d34cfb69ec07c70a4a6b2a9ab39
Lars1702 <Lars1702@gmail.com>
2020-07-01 14:52:29 +0200

    Added continuous integration and delivery

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import React from "react"
 import logo from "./logo.svg"
 import "./App.css"
-//adding comment to test Git Credential Manager
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Testing CI/CD system</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   )
