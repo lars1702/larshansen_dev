1f0dff85da3e2ae222db2cd7be12ab69ecae4605
Lars1702 <Lars1702@gmail.com>
2020-07-01 14:15:31 +0200

    test git credential change

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react"
 import logo from "./logo.svg"
 import "./App.css"
-
+//adding comment to test Git Credential Manager
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Testing CI system</p>
+        <p>Testing CI/CD system</p>
         <a
           className="App-link"
           href="https://reactjs.org"
