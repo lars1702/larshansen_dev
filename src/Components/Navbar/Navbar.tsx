bbe939fe931faf257175f55e277ba08963b2f1fe
lars1702 <lars1702@gmail.com>
2021-10-29 11:01:08 +0100

    rm script stuff

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -3,10 +3,11 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import About from "../About"
 import CV from "../CV"
 import Home from "../Home"
-import ScriptComponent from "../ScriptComponent"
 import { StyledNav, StyledLink } from "./Navbar-styles"
 
 type useStateType = [number, React.Dispatch<React.SetStateAction<number>>]
+
+const pages = ["/", "/about", "/cv", "/"]
 
 const NavBar = (): JSX.Element => {
   const [activePage, setActivePage]: useStateType = useState<number>(0)
@@ -23,15 +24,11 @@ const NavBar = (): JSX.Element => {
         <StyledLink onClick={() => setActivePage(2)} active={activePage === 2} to="/cv">
           CV
         </StyledLink>
-        <StyledLink onClick={() => setActivePage(3)} active={activePage === 3} to="/scripts">
-          SCRIPTS
-        </StyledLink>
       </StyledNav>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/about" component={About} />
         <Route path="/cv" component={CV} />
-        <Route path="/scripts" component={ScriptComponent} />
       </Switch>
     </Router>
   )
